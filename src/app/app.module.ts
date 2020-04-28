3f2cb533038cf4ce5e616dc0bbee3feb597f68a5
Joao Paulo <joaopaulloojpos15@gmail.com>
2020-04-28 17:50:57 -0300

    configurando a aplicacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import localePtExtra from '@angular/common/locales/extra/pt';
+import { DEFAULT_CURRENCY_CODE } from '@angular/core';
+
+registerLocaleData(localePt, 'pt', localePtExtra);
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +18,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'pt',
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
